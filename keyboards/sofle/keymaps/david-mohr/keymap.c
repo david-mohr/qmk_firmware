450189336aeff5e66f3b3c79aeb14419540462ac
David Mohr <david.mohr@ardexa.com>
2023-01-11 09:17:44 +1100

    0, 1, 2, 3 are used far more often, so move them to the home row

--- keyboards/sofle/keymaps/david-mohr/keymap.c
+++ keyboards/sofle/keymaps/david-mohr/keymap.c
@@ -36,8 +36,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_NUM_NAV] = LAYOUT(
   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                       XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
   XXXXXXX,  XXXXXXX,   KC_7,     KC_8,     KC_9,    XXXXXXX,                       XXXXXXX,  XXXXXXX,  KC_PGUP,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-  XXXXXXX,  KC_DOT,    KC_4,     KC_5,     KC_6,    XXXXXXX,                       KC_LEFT,  KC_DOWN,   KC_UP,   KC_RGHT,  XXXXXXX,  XXXXXXX,
-  XXXXXXX,   KC_0,     KC_1,     KC_2,     KC_3,    XXXXXXX, _______,     _______, XXXXXXX,  XXXXXXX,  KC_PGDN,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+  XXXXXXX,   KC_0,     KC_1,     KC_2,     KC_3,    XXXXXXX,                       KC_LEFT,  KC_DOWN,   KC_UP,   KC_RGHT,  XXXXXXX,  XXXXXXX,
+  XXXXXXX,  KC_DOT,    KC_4,     KC_5,     KC_6,    XXXXXXX, _______,     _______, XXXXXXX,  XXXXXXX,  KC_PGDN,  XXXXXXX,  XXXXXXX,  XXXXXXX,
   XXXXXXX,  XXXXXXX,                      _______, __HELD__, __HELD__,     _______, _______, _______,             XXXXXXX,  XXXXXXX
 ),
 [_SYM] = LAYOUT(
